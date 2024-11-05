29c72955b15781d2e30374f3613386df0eca5151
IlhanYapici <ilhan-yapici@outlook.fr>
2024-11-05 08:42:16 +0000

    fix: update @1schoolone/ui to fix dev server issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
+import { AuthLayout } from '@1schoolone/ui'
+import '@1schoolone/ui/dist/index.css'
+
 export function App() {
-	return <></>
+	return (
+		<AuthLayout>
+			<h1>hello world!</h1>
+		</AuthLayout>
+	)
 }
