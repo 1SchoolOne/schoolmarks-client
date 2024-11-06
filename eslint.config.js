90c8dccca78334918c9e050ffafc53615f797d38
IlhanYapici <ilhan-yapici@outlook.fr>
2024-11-06 22:49:51 +0000

    chore(eslint): update eslint config and set eslint as default formatter

--- eslint.config.js
+++ eslint.config.js
@@ -1,42 +1,47 @@
-import js from '@eslint/js'
-import prettier from 'eslint-config-prettier'
+import eslint from '@eslint/js'
 import jsdoc from 'eslint-plugin-jsdoc'
+import eslintPluginPrettier from 'eslint-plugin-prettier/recommended'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import globals from 'globals'
 import tseslint from 'typescript-eslint'
 
-export default tseslint.config(
-	{ ignores: ['dist'] },
-	{
-		extends: [
-			js.configs.recommended,
-			...tseslint.configs.recommended,
-			jsdoc.configs['flat/stylistic-typescript'],
-			prettier,
-		],
-		files: ['**/*.{ts,tsx}'],
-		languageOptions: {
-			ecmaVersion: 2020,
-			globals: globals.browser,
-		},
-		plugins: {
-			'react-hooks': reactHooks,
-			'react-refresh': reactRefresh,
-		},
-		rules: {
-			...reactHooks.configs.recommended.rules,
-			'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-			'@typescript-eslint/no-unused-vars': [
-				'error',
-				{
-					args: 'all',
-					argsIgnorePattern: '^_',
-				},
-			],
-			'@typescript-eslint/no-require-imports': 'error',
-			'@typescript-eslint/no-explicit-any': 'warn',
-			semi: ['error', 'never'],
-		},
+export default tseslint.config({
+	files: ['src/**/*.{ts,tsx}'],
+	extends: [
+		eslint.configs.recommended,
+		...tseslint.configs.recommended,
+		jsdoc.configs['flat/stylistic-typescript'],
+		eslintPluginPrettier,
+	],
+	languageOptions: {
+		ecmaVersion: 2020,
+		globals: globals.browser,
 	},
-)
+	plugins: {
+		'react-hooks': reactHooks,
+		'react-refresh': reactRefresh,
+	},
+	rules: {
+		...reactHooks.configs.recommended.rules,
+		'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+		'no-unused-vars': 'off',
+		'@typescript-eslint/no-unused-vars': [
+			'error',
+			{
+				args: 'all',
+				argsIgnorePattern: '^_',
+				caughtErrors: 'all',
+				caughtErrorsIgnorePattern: '^_',
+				destructuredArrayIgnorePattern: '^_',
+				varsIgnorePattern: '^_',
+				ignoreRestSiblings: true,
+			},
+		],
+		'no-unused-expressions': 'off',
+		'@typescript-eslint/no-unused-expressions': ['error', { allowShortCircuit: true }],
+		'@typescript-eslint/no-require-imports': 'error',
+		'@typescript-eslint/no-explicit-any': 'warn',
+		semi: ['error', 'never'],
+	},
+})
