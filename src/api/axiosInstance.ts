4913e6005bcb98dbbdb022b20024ecbc536f34ec
IlhanYapici <ilhan-yapici@outlook.fr>
2024-12-12 17:27:04 +0000

    refactor(csrf): remove csrf token references
    The API no longer requires a csrf token

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,22 +1,10 @@
 import axios from 'axios'
 
-import { getCookie } from '@utils/getCookie'
-
 const API_HOST = import.meta.env.VITE_API_HOST
 
 const axiosInstance = axios.create({
 	baseURL: API_HOST ?? 'http://127.0.0.1:8000',
 	withCredentials: true,
-})
-
-axiosInstance.interceptors.request.use((config) => {
-	const token = getCookie('csrftoken')
-
-	if (token.length > 0) {
-		config.headers['X-CSRFToken'] = token
-	}
-
-	return config
 })
 
 export { axiosInstance }
