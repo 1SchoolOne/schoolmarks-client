d5bf0052e55f2dbf9ab660d99f8c211804d598d9
Nico <nicolas.olivier@edu.esiee-it.fr>
2024-12-19 14:15:51 +0000

    feat/grades init ui filter for grade

--- src/routes/grades/index.tsx
+++ src/routes/grades/index.tsx
@@ -1,12 +1,22 @@
+import { Outlet } from 'react-router-dom'
+
 import { Route } from '@types'
+
+import { Grades } from './grades'
 
 export const gradesRoute: Route = {
 	path: 'grades',
-	element: 'Notes',
+	element: <Outlet />,
 	handle: {
 		crumb: {
 			label: 'Notes',
 			path: 'grades',
 		},
 	},
+	children: [
+		{
+			index: true,
+			element: <Grades />,
+		},
+	],
 }
