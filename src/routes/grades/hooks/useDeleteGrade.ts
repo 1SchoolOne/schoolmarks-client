b6ba1c76acedaa1892ef5bc8ebb792e99856184c
MathieuDML <mathieudamotalongo@gmail.com>
2025-02-19 14:59:25 +0000

    feat : ajout de la fonctionnalité de suppression des évaluations avec confirmation et gestion des notes associées

---
+++ src/routes/grades/hooks/useDeleteGrade.ts
@@ -0,0 +1,36 @@
+import { Modal, message } from 'antd'
+
+import { deleteGrade } from '@api/grades'
+
+import { GradeWithUser } from './useGradesData'
+
+interface UseDeleteGradeProps {
+	onSuccess?: () => Promise<void>
+}
+
+export function useDeleteGrade({ onSuccess }: UseDeleteGradeProps = {}) {
+	const handleDelete = async (grade: GradeWithUser) => {
+		Modal.confirm({
+			title: 'Êtes-vous sûr de vouloir supprimer cette évaluation ?',
+			content: 'Cette action supprimera également toutes les notes associées aux étudiants.',
+			okText: 'Supprimer',
+			okType: 'danger',
+			cancelText: 'Annuler',
+			async onOk() {
+				try {
+					if (!grade.id) throw new Error("ID de l'évaluation non défini")
+					await deleteGrade(grade.id)
+					message.success('Évaluation supprimée avec succès')
+					if (onSuccess) {
+						await onSuccess()
+					}
+				} catch (error) {
+					message.error("Erreur lors de la suppression de l'évaluation")
+					console.error(error)
+				}
+			},
+		})
+	}
+
+	return { handleDelete }
+}
