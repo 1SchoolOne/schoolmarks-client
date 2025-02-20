fc6d8493dc38ebb231786f63c9744cac9a18f478
IlhanYapici <ilhan-yapici@outlook.fr>
2025-02-20 00:34:06 +0000

    corrections mineures

--- src/routes/grades/Grade.tsx
+++ src/routes/grades/Grade.tsx
@@ -1,65 +1,16 @@
-import { Alert, Flex, Spin } from 'antd'
-import { useEffect, useState } from 'react'
+import { useContext } from 'react'
 
-import { getSession } from '@api/auth'
+import { IdentityContext } from '@contexts'
 
 import { GradesStudent } from './componants/GradesStudent'
 import { GradesTeacher } from './componants/GradesTeacher'
 
 export const Grade = () => {
-	const [userRole, setUserRole] = useState<string | null>(null)
-	const [loading, setLoading] = useState<boolean>(true)
-	const [error, setError] = useState<string | null>(null)
+	const { user } = useContext(IdentityContext)
 
-	useEffect(() => {
-		const fetchUserSession = async () => {
-			try {
-				setLoading(true)
-				const sessionData = await getSession()
-				const user = sessionData.data.user
-
-				if (!user) {
-					setError('Utilisateur non connecté')
-					return
-				}
-
-				setUserRole(user.role)
-				console.log('Utilisateur connecté avec le rôle:', user.role)
-			} catch (err) {
-				console.error('Erreur lors de la récupération de la session:', err)
-				setError('Impossible de récupérer les informations de session')
-			} finally {
-				setLoading(false)
-			}
-		}
-
-		fetchUserSession()
-	}, [])
-
-	if (loading) {
-		return (
-			<Flex justify="center" align="center" style={{ height: '100vh' }}>
-				<Spin size="large" tip="Chargement des données..." />
-			</Flex>
-		)
+	if (user?.role === 'student') {
+		return <GradesStudent />
 	}
 
-	if (error) {
-		return (
-			<Alert
-				message="Erreur"
-				description={error}
-				type="error"
-				showIcon
-				style={{ maxWidth: '600px', margin: '20px auto' }}
-			/>
-		)
-	}
-
-	switch (userRole) {
-		case 'teacher':
-			return <GradesTeacher />
-		case 'student':
-			return <GradesStudent />
-	}
+	return <GradesTeacher />
 }
